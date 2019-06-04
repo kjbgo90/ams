1558758f8c08e8baf4be4bffc7a89ec564ba10ba
nnn <rjsckdd12@gmail.com>
2019-06-04 13:54:13 +0900

    건창 1st commit:plus Vo

---
+++ src/main/java/net/ourams/vo/CourseDataroomVo.java
@@ -0,0 +1,36 @@
+package net.ourams.vo;
+
+public class CourseDataroomVo {
+	/*
+	 * data room 
+	 */
+	private int dataRoomNo;
+	private int courseNo;
+	private String dataRoomName;
+	private int pRoonNo;
+	/*
+	 * data room file
+	 */
+	private int dataRoomFileNo;
+	/* private int dataRoomNo; */
+	private int fileNo;
+	
+	/*
+	 * data room file tag
+	 */
+	private int dataRoomFileTagNo;
+	/*
+	 * private int dataRoomFileNo;
+	 * private int dataTagNo;
+	 */
+	
+	/*
+	 * data tag 
+	 */
+	private int dataTagNo;
+	private String dataTagName;
+	
+	
+	
+	
+}
