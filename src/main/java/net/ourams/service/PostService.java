aa81c4f9566c78548aa63108aa3a989a6842943f
bit <bit@DESKTOP-U2AQ5A3>
2019-06-13 17:06:56 +0900

    boni 9th commit : page

--- src/main/java/net/ourams/service/PostService.java
+++ src/main/java/net/ourams/service/PostService.java
@@ -17,7 +17,7 @@ public class PostService {
 	private PostDao postDao;
 	
 	public Map<String, Object> selectPostPaging( int pageNo){
-		int listSize = 5 ;
+		int listSize = 10 ;
 		int pageNo1 = 1+listSize*(pageNo-1);
 		int pageNo2 = listSize*pageNo;
 		int countPage = postDao.countPost();
